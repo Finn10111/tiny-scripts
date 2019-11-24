ca6f0c83aef2d985512a90b0d99ae9190a170c18
Finn Christiansen <kontakt@finnchristiansen.de>
2019-11-24 17:07:21 +0100

    support other resolutions for video converting

--- video_convert.sh
+++ video_convert.sh
@@ -13,8 +13,11 @@ case "$RESOLUTION" in
 	"720" )
 		echo "720 ok"
 		;;
+	"480" )
+		echo "480 ok"
+		;;
 	* )
-		echo "invalid resolution (1080/720 only)"
+		echo "invalid resolution (1080/720/480 only)"
 		exit
 		;;
 esac
@@ -24,8 +27,10 @@ case "$FRAMERATE" in
 		;;
 	"30" )
 		;;
+	"25" )
+		;;
 	* )
-		echo "invalid framerate (30/60 only)"
+		echo "invalid framerate (30/60/25 only)"
 		exit
 		;;
 esac
@@ -34,7 +39,11 @@ EXTENSION="${INPUT_FILE##*.}"
 OUTPUT_FILE=$(basename "$INPUT_FILE" | sed "s/\.$EXTENSION/ "$RESOLUTION"p$FRAMERATE.$EXTENSION/")
 
 if [ -f "$INPUT_FILE" ] && [ ! -f "$OUTPUT_FILE" ]; then
-	ffmpeg -i "$INPUT_FILE" -vf scale=-1:$RESOLUTION -c:v libx264 -r $FRAMERATE -crf $CRF -preset $PRESET -c:a copy "$OUTPUT_FILE"
+        if [ "$RESOLUTION" == "480" ]; then
+            ffmpeg -i "$INPUT_FILE" -vf scale=854:480 -c:v libx264 -r $FRAMERATE -crf $CRF -preset $PRESET -c:a copy "$OUTPUT_FILE"
+        else
+            ffmpeg -i "$INPUT_FILE" -vf scale=-1:$RESOLUTION -c:v libx264 -r $FRAMERATE -crf $CRF -preset $PRESET -c:a copy "$OUTPUT_FILE"
+        fi
 	if [ $? -eq 0 ]; then
 		notify-send "Video erfolgreich konvertiert: $OUTPUT_FILE"
 	else
