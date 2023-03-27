6c529dea722270ed5e1955fc3f5ef4aa406da671
Finn Christiansen <kontakt@finnchristiansen.de>
2023-03-27 21:22:52 +0200

    added script to determine if Kodi is idle

---
+++ isKodiIdle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+def is_kodi_idle(host, idletime):
+    username = 'kodi'
+    password = 'kodi'
+    payload = {
+            'jsonrpc': '2.0',
+            'method': 'XBMC.GetInfoBooleans',
+            'params':
+                {'booleans': ['System.IdleTime({})'.format(idletime)] },
+            'id': 1}
+    try:
+        r = requests.post('http://{}:8080/jsonrpc'.format(host), json=payload, auth=(username, password))
+        result = r.json()['result']
+        return bool(next(iter(result.values())))
+    except Exception:
+        return True
+
+
+def main():
+    idletime = 7200
+    hosts = ['libreelec-wohnzimmer', 'libreelec-schlafzimmer']
+    active_kodis = 0
+
+    for host in hosts:
+        idle = is_kodi_idle(host, idletime)
+        if not idle:
+            active_kodis += 1
+    if active_kodis > 0:
+        return 1
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    # exit with 0 if no kodi is active
+    sys.exit(main())
